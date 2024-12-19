324b5ab3e346021486f2e4b9d8d6c071be371d20
Maxime Croisier <maxime.croisier@etu.univ-littoral.fr>
2024-12-19 17:11:03 +0100

    Add Spring to project

---
+++ src/test/java/fr/univlittoral/whattsup/WhattsUpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univlittoral.whattsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattsUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
